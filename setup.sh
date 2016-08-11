60c7b08ef0abf3aad31b4734626a80e5a09acbb5
Jørgen Wahlberg <jorgen@wahlberg.no>
2016-08-11 15:11:44 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,16 +9,15 @@
 # don't start window manager by default
 systemctl set-default multi-user.target
 
-# update some packages we need
+# add ubuntu repo where we can find chromium-browser package
+echo "deb http://ppa.launchpad.net/canonical-chromium-builds/stage/ubuntu vivid main” > /etc/apt/sources.list.d/chromium-ppa.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DB69B232436DAC4B50BDC59E4E1B983C5B393194
+
+# update packages and install chromium-browser and some dependencies
 apt-get -y update
 apt-get -y dist-upgrade
 apt-get -y install matchbox x11-xserver-utils ttf-mscorefonts-installer xwit sqlite3
-
-# install chromium
-echo "deb http://ppa.launchpad.net/canonical-chromium-builds/stage/ubuntu vivid main” > /etc/apt/sources.list.d/chromium-ppa.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DB69B232436DAC4B50BDC59E4E1B983C5B393194
-apt update
-apt install chromium-browser
+apt-get -y install chromium-browser
 
 # set time zone to Norwegian time
 sudo cp /usr/share/zoneinfo/posix/Europe/Oslo /etc/localtime
