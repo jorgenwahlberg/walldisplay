0618ff92917e3df8e2ccfc71dab07835423c568b
Jørgen Veum-Wahlberg <jorgen.wahlberg@amedia.no>
2018-11-08 08:49:08 +0100

    added hints on read-only filesystem and updated setup.sh to use 2018-10-09-raspbian-stretch-lite.img

--- setup.sh
+++ setup.sh
@@ -14,20 +14,12 @@ sudo dphys-swapfile swapoff
 sudo dphys-swapfile uninstall
 sudo update-rc.d dphys-swapfile remove
 
-# add ubuntu repo where we can find chromium-browser package
-# see https://raspberrypi.stackexchange.com/questions/44384/how-to-get-chromium-on-raspberry-3 for chromium details
-echo "deb http://ports.ubuntu.com trusty main universe multiverse" > /etc/apt/sources.list.d/ubuntu.list
-echo "deb http://ports.ubuntu.com trusty-updates main universe multiverse" >> /etc/apt/sources.list.d/ubuntu.list
-echo 'APT::Default-Release "jessie";' > /etc/apt/apt.conf.d/10release
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 40976EAF437D05B5 3B4FE6ACC0B21F32
-
-
 # update packages and install chromium-browser and some dependencies
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y purge apt-listchanges
 apt-get -y update
 apt-get -y dist-upgrade
-apt-get -y install xinit matchbox xserver-xorg xserver-xorg-legacy x11-xserver-utils ttf-mscorefonts-installer xwit sqlite3 urlwatch chromium-browser/trusty chromium-browser-l10n/trusty chromium-codecs-ffmpeg-extra/trusty
+apt-get -y install xinit matchbox xserver-xorg xserver-xorg-legacy x11-xserver-utils ttf-mscorefonts-installer xwit sqlite3 urlwatch chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg-extra
 
 # set time zone to Norwegian time
 sudo cp /usr/share/zoneinfo/posix/Europe/Oslo /etc/localtime
@@ -46,4 +38,3 @@ echo "allowed_users=anybody" > /etc/X11/Xwrapper.config
 # reboot
 echo Please reboot your device:
 echo \$ sudo reboot
-
